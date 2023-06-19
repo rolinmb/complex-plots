fc738d9ff56f6cfed4abebe7b83f5a030742327d
Rolin Blake <rolinmblake@gmail.com>
2023-06-19 18:35:44 -0500

    small styling changes

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ function inputSubmitHandle(event){
 	let X = parseFloat(x) || 0;
 	let Y = parseFloat(y) || 0;
 	z_in = math.complex(X,Y);
-	console.log("z = "+z_in.toString());
-	document.getElementById("input-display").innerHTML = "INPUT: z = "+z_in.toString();
+	document.getElementById("input-display").innerHTML = "z = "+z_in.toString();
 	var inputPlane = document.getElementById("z-input");
 	let inputData = [{
 		x: [z_in.re],
@@ -32,7 +31,7 @@ function fzEntryHandle(event){
 	event.preventDefault();
 	let fz_string = document.getElementById("function-string").value;
 	if(fz_string === ""){ return; }
-	document.getElementById("function-display").innerHTML = "* f(z) = "+fz_string;
+	document.getElementById("function-display").innerHTML = "f(z) = "+fz_string;
 	const parsed_expr = math.parse(fz_string);
 	if(typeof z_in === 'undefined'){
 		z_in = math.complex(0,0);
@@ -41,8 +40,7 @@ function fzEntryHandle(event){
 	const scope = { z };
 	const result = parsed_expr.evaluate(scope);
 	z_out = math.complex(result.re,result.im);
-	console.log("f("+z_in.toString()+") = "+z_out.toString());
-	let new_text = "* f("+z_in.toString()+") = "+z_out.toString();
+	let new_text = "f("+z_in.toString()+") = "+z_out.toString();
 	document.getElementById("function-eval").innerHTML = new_text;
 	var outputPlane = document.getElementById("z-output");
 	const outputData = [{
